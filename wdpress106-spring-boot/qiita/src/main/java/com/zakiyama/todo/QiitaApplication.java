95fac0edf2079e87edb557a6c5cbee12e4e8f0b6
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-11-06 21:51:16 +0900

    Generate boilarplate

---
+++ wdpress106-spring-boot/qiita/src/main/java/com/zakiyama/todo/QiitaApplication.java
@@ -0,0 +1,12 @@
+package com.zakiyama.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QiitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QiitaApplication.class, args);
+	}
+}
