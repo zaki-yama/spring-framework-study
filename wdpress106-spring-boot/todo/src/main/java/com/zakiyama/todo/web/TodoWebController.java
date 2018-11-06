7b0bd45a3a8c5bc9d2e2530ad48302dfd68a2f24
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-11-06 15:03:02 +0900

    Implement readOneTask

--- wdpress106-spring-boot/todo/src/main/java/com/zakiyama/todo/web/TodoWebController.java
+++ wdpress106-spring-boot/todo/src/main/java/com/zakiyama/todo/web/TodoWebController.java
@@ -2,6 +2,7 @@ package com.zakiyama.todo.web;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import com.zakiyama.todo.domain.Task;
 import com.zakiyama.todo.form.TaskForm;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -66,5 +68,41 @@ public class TodoWebController {
     Boolean hasDone = form.getHasDone();
     Task task = new Task(subject, deadLine, hasDone);
     todoService.createTask(task);
+  }
+
+  // タスクを1件取得
+  @GetMapping(value = "tasks/{id}")
+  public ModelAndView readOneTask(
+      // アノテーションをつけ、URL 中の変数名と揃えることで
+      // URL のパラメータから id の値を取得できる
+      @PathVariable Integer id) {
+    Optional<TaskForm> form = readTaskFromId(id);
+    if (!form.isPresent()) {
+      return new ModelAndView(REDIRECT_TO);
+    }
+    ModelAndView modelAndView = toTasksPage();
+    modelAndView.addObject("taskId", id);
+    modelAndView.addObject("form", form.get());
+    List<Task> tasks = todoService.findAllTasks();
+    modelAndView.addObject(TASKS, tasks);
+    return modelAndView;
+  }
+
+  private Optional<TaskForm> readTaskFromId(Integer id) {
+    Optional<Task> task = todoService.findOneTask(id);
+    if (!task.isPresent()) {
+      return Optional.ofNullable(null);
+    }
+
+    String formSubject = task.get().getSubject();
+    LocalDate formDeadLine = task.get().getDeadLine();
+    Boolean hasDone = task.get().getHasDone();
+    Boolean isNewTask = false;
+    TaskForm form = new TaskForm(
+      formSubject,
+      formDeadLine,
+      hasDone,
+      isNewTask);
+    return Optional.ofNullable(form);
   }
 }
