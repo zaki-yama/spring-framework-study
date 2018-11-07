8a5f48e04440de04f65252115428e86c19653ed1
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-11-07 16:29:52 +0900

    Add QiitaScheduleController

---
+++ wdpress106-spring-boot/qiita/src/main/java/com/zakiyama/qiita/web/QiitaScheduleController.java
@@ -0,0 +1,19 @@
+package com.zakiyama.qiita.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class QiitaScheduleController {
+
+  private static final long INTERVAL = 900_000;
+  
+  @Autowired
+  QiitaService qiitaService;
+
+  @Scheduled(fixedRate = INTERVAL)
+  public void crowlQiitaItems() {
+    qiitaService.crawlItems();
+  }
+}
