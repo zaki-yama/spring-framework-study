5e01faa7545edba4d30d6807b9a7e4963b127b9d
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-11-08 05:09:32 +0900

    Add `@Controller` annotation to QiitaWebController

--- wdpress106-spring-boot/qiita/src/main/java/com/zakiyama/qiita/web/QiitaWebController.java
+++ wdpress106-spring-boot/qiita/src/main/java/com/zakiyama/qiita/web/QiitaWebController.java
@@ -6,9 +6,11 @@ import com.zakiyama.qiita.domain.QiitaItem;
 import com.zakiyama.qiita.service.QiitaService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class QiitaWebController {
 
   @Autowired
