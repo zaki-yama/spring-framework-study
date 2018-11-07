fef9a215fb47c71718a715baa5e2db390cbbfa21
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2018-11-07 16:12:12 +0900

    Add @EnableScheduling

--- wdpress106-spring-boot/qiita/src/main/java/com/zakiyama/qiita/QiitaApplication.java
+++ wdpress106-spring-boot/qiita/src/main/java/com/zakiyama/qiita/QiitaApplication.java
@@ -2,8 +2,10 @@ package com.zakiyama.qiita;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class QiitaApplication {
 
 	public static void main(String[] args) {
